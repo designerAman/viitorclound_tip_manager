95c1e2b47f72883f2782501678876701f30fd44d
Aman2601 <debugger.aman@gmail.com>
2021-10-24 00:52:59 +0530

    create users table

---
+++ src/database-migrations/20211023212929-create-users-table.js
@@ -0,0 +1,43 @@
+const Sequelize = require("sequelize");
+
+module.exports = {
+  up: async (query) => {
+    await query.createTable("users", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        unique: true,
+      },
+      profilePicture: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      accessToken: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+    });
+  },
+  down: async (query) => {
+    await query.dropTable("users");
+  },
+};
